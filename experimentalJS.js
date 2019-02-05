888b9deeb3a1604fc07de568e5b9003ffc02a061
dollarman <carlos.dollarman+github@gmail.com>
2019-02-05 01:45:58 -0400

    color and text size update

---
+++ experimentalJS.js
@@ -0,0 +1,10 @@
+<script type="text/javascript">
+  let links = {"Github": "https://github.com/Dollarman"};
+  let icon = {"Github": "fa-github"};
+  let s = "";
+  for (let e in links) {
+    s += '<!--' + e + '-->\
+    <p class="link light-teal">\
+      <a href="' + links[e] + '" target="_blank"><i class="fa ' + icon[e] + ' fa-fw size-xl nomargin-r" aria-hidden="true"></i> Github </a></p>'
+  }
+</script>
